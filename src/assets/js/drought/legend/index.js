9f01fa1d90bfb3ed0e3687fb5bc2ee76c8229fdb
Chris Mahlke <cmahlke@esri.com>
2021-08-03 12:50:46 -0700

    Stable

--- src/assets/js/drought/legend/index.js
+++ src/assets/js/drought/legend/index.js
@@ -1,36 +1,38 @@
 import { loadModules } from 'esri-loader';
 
-export function init(params) {
-    addLegendWidget(params.view, params.position);
-}
+export async function init(params) {
+    return await new Promise((resolve, reject) => {
+        loadModules([
+            "esri/widgets/Legend"
+        ]).then(([Legend]) => {
+            const {
+                map: {
+                    allLayers: {
+                        items = {items: []}
+                    }
+                },
+            } = params.view;
 
+            let agrLayer = items.filter(layer => {
+                return layer.title === "TotalAgSales Centroids - copy";
+            });
 
-function addLegendWidget(view, position) {
-    loadModules([
-        "esri/widgets/Legend"
-    ]).then(([Legend]) => {
-        const {
-            map : {
-                allLayers: {
-                    items = { items: [] }
-                }
-            },
-        } = view;
-
-        let agrLayer = items.filter(layer => {
-            return layer.title === "TotalAgSales Centroids - copy";
+            if (agrLayer.length > 0) {
+                let widget = new Legend({
+                    view: params.view,
+                    layerInfos: [{
+                        layer: agrLayer[0]
+                    }]
+                });
+                params.view.ui.add(widget, {
+                    position: params.position
+                });
+                widget.when(() => {
+                    resolve(widget)
+                }, error => {
+                    resolve(error);
+                });
+            }
         });
-
-        if (agrLayer.length > 0) {
-            let legendWidget = new Legend({
-                view: view,
-                layerInfos: [{
-                    layer: agrLayer[0]
-                }]
-            });
-            view.ui.add(legendWidget, {
-                position: position
-            });
-        }
     });
 }
